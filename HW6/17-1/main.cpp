6f3f131f8c15a7cab87ff024691c9e79f5572e11
B10213147 <B10213147@yuntech.org.tw>
2017-06-14 01:17:03 +0800

    Added 17-1

---
+++ HW6/17-1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    fstream fp;
+    fp.open("abc.txt", fstream::in);
+    char line[100];
+    fp.getline(line, 100, '\t');
+    fp.close();
+    cout << "Data in abc.txt:" << endl;
+    cout << line << endl;
+    cout << "After remove number:" << endl;
+    for(int i = 0; line[i] != 0; i++){
+        if(!(line[i] >= '0' && line[i] <= '9')){
+            cout << line[i];
+        }
+    }
+    return 0;
+}
