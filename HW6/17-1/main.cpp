e00b12fc46333186f685e25133e7aaaeeeb2ed5a
B10213147 <B10213147@yuntech.org.tw>
2017-06-14 10:55:27 +0800

    Added file stream check

--- HW6/17-1/main.cpp
+++ HW6/17-1/main.cpp
@@ -7,12 +7,13 @@ int main()
 {
     fstream fp;
     fp.open("abc.txt", fstream::in);
+    if(!fp){ return -1; }
     char line[100];
     fp.getline(line, 100, '\t');
     fp.close();
     cout << "Data in abc.txt:" << endl;
     cout << line << endl;
-    cout << "After remove number:" << endl;
+    cout << "After remove numbers:" << endl;
     for(int i = 0; line[i] != 0; i++){
         if(!(line[i] >= '0' && line[i] <= '9')){
             cout << line[i];
